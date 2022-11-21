5acdbad15ba8966b2518c72ac2fdc809b4750139
Diego Acosta <djacosta.07@gmail.com>
2022-11-21 12:37:47 -0800

    Got firebase authentification working

--- keck-escape-room/src/components/App.js
+++ keck-escape-room/src/components/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import { SignIn, SignOut, useAuthentication } from "../services/authService";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Welcome! Enter below, if you dare.
-        <button class="logInButton">Enter the depths of the Keck Lab</button>
+        <SignIn />
+        {/* <button class="logInButton">Enter the depths of the Keck Lab</button> */}
       </header>
     </div>
   );
--- keck-escape-room/src/firebaseConfig.js
+++ keck-escape-room/src/firebaseConfig.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,5 +12,6 @@ const firebaseConfig = {
   appId: "1:250104678734:web:2bc78946b6d5e9ed0db61f",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
