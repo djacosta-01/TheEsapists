235f851e9710d4e90364d05649076575c7ba4ebe
Relectrik <viv@dluvs.com>
2022-11-21 11:32:00 -0800

    Log-in Page CSS complete

--- keck-escape-room/src/App.js
+++ keck-escape-room/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Welcome! Enter below.
+        Welcome! Enter below, if you dare.
         <button class="logInButton">Enter the depths of the Keck Lab</button>
       </header>
     </div>
