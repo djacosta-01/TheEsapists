39ea4d5c59e2bffe2c3a887bbfc67fa1d50ae560
kimberlykubo <kimberly.n.kubo@gmail.com>
2022-11-28 11:53:48 -0800

    added gameService

---
+++ keck-escape-room/src/services/gameService.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+
+export function Game() {
+  return (
+    <div id="game_one">
+      <p>Game 1</p>
+    </div>
+  );
+}
