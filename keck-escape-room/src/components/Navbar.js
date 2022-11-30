29b3a1d02f24b68dd9c81e453f902e47288d4e9b
Diego Acosta <djacosta.07@gmail.com>
2022-11-30 11:12:36 -0800

    imported css...

--- keck-escape-room/src/components/Navbar.js
+++ keck-escape-room/src/components/Navbar.js
@@ -1,12 +1,11 @@
 import { SignOut } from "../services/authService";
-
+import "./NavBar.css";
 export default function NavBar() {
   return (
-    <div id="nav-header">
-      <div id="nav-bar-container">
-        <a href="#">Home</a>
-        <a href="##">Load Game</a>
-      </div>
+    <div id="nav-bar-container">
+      This is the container div
+      {/* <a href="#">Home</a>
+        <a href="##">Load Game</a> */}
     </div>
   );
 }
