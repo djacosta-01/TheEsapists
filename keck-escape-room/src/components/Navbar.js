b09b5c3cb3a4bcc019881e22168e39dca611499e
Relectrik <viv@dluvs.com>
2022-12-15 18:57:53 -0800

    added better responsiveness

--- keck-escape-room/src/components/Navbar.js
+++ keck-escape-room/src/components/Navbar.js
@@ -1,10 +1,10 @@
-import { SignOut } from "../services/authService";
 import "./NavBar.css";
 export default function NavBar() {
   return (
     <div id="nav-bar-container">
       <a href="#">Home</a>
       <a href="##">About</a>
+      <a href="###">Open Map</a>
     </div>
   );
 }
