840f56a8e7d562cb87f7e80f4d3a077296d7aa2c
Relectrik <viv@dluvs.com>
2022-12-15 21:25:44 -0800

    pre routing changes

--- keck-escape-room/src/components/Navbar.js
+++ keck-escape-room/src/components/Navbar.js
@@ -2,9 +2,8 @@ import "./NavBar.css";
 export default function NavBar() {
   return (
     <div id="nav-bar-container">
-      <a href="#">Home</a>
+      <a href="corner">Head to the corner</a>
       <a href="##">About</a>
-      <a href="###">Open Map</a>
     </div>
   );
 }
