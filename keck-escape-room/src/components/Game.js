07c9a0047b8b93d020fb5ad6b5806ee5029bd7f1
Masao Kitamura <masaok@gmail.com>
2022-12-16 03:05:58 -0800

    Fixing Game Date display

--- keck-escape-room/src/components/Game.js
+++ keck-escape-room/src/components/Game.js
@@ -1,16 +1,19 @@
+import { DateTime } from 'luxon'
+
 export default function Game({ game }) {
+  const dt = DateTime.fromSeconds(game.date.seconds)
+
   return (
     <div id="game-container">
-      This the game component
       {!game ? (
         <p>No game selected</p>
       ) : (
         <section>
           <h2>{game.name}</h2>
-          <p id="date">{`Created: ${game.date}`}</p>
+          <p id="date">{`Created: ${dt.toLocaleString(DateTime.DATE_SHORT)}`}</p>
           <p id="clues">{`Clues found: ${game.clues}`}</p>
         </section>
       )}
     </div>
-  );
+  )
 }
