617eb80f3ae1fe93342526489f286bd7bebb743a
Relectrik <viv@dluvs.com>
2022-12-15 23:04:28 -0800

    more functionality

--- keck-escape-room/src/components/Navbar.js
+++ keck-escape-room/src/components/Navbar.js
@@ -1,8 +1,14 @@
 import "./NavBar.css";
+import { Link } from "react-router-dom";
 export default function NavBar() {
+  // let navigate = useNavigate();
+  // const routeChange = () => {
+  //   let path = "corner";
+  //   navigate(path);
+  // };
   return (
     <div id="nav-bar-container">
-      <a href="corner">Head to the corner</a>
+      <a href="corner"> Head to the corner</a>
       <a href="##">About</a>
     </div>
   );
